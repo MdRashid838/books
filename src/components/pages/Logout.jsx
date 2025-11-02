6eff46275c8c1ca3c745a8178ec5d6fb7e1b12d0
Anwarul07 <Officialmdanwarulhaque@gmail.com>
2025-11-02 16:23:45 +0530

    update

--- src/components/pages/Logout.jsx
+++ src/components/pages/Logout.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Logout = () => {
   return (
-    <div>
-      Logout
+    <div className="flex justify-center items-center h-screen text-3xl font-bold h-screen">
+      <P>Logout</P>
     </div>
-  )
-}
+  );
+};
 
 export default Logout;
