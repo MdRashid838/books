7758d8307e6ce069dc55ed1eaae6261453b5e57a
Anwarul07 <Officialmdanwarulhaque@gmail.com>
2025-11-02 16:15:13 +0530

    shimmer

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import {Link, useParams} from 'react-router-dom';
+import { Link, useParams } from "react-router-dom";
 import { FaCartPlus } from "react-icons/fa";
+import useOnline from "./useOnline";
+import Shimmer from "./Shimmer";
 
 const Home = () => {
   const [data, setData] = useState([]);
-  const [loading,setLoading] = useState(false)
 
   const dataItem = async () => {
     let url = "https://anwarbook.onrender.com/api/books/";
@@ -12,36 +13,44 @@ const Home = () => {
     let jdata = await api.json();
     // console.log(jdata)
     setData(jdata);
-    setLoading()
   };
   useEffect(() => {
     dataItem();
   }, []);
 
-  if(loading)
-    return <div>
-      loading......
-    </div>
-  return (
-    <div className="grid lg:grid-flow-row lg:grid-cols-5 md:grid-cols-3 lg:row-span-3 lg:p-10 p-6 lg:gap-8 gap-5 m-auto items-center ">
+  const isOnline = useOnline();
+  if (!isOnline) {
+    return (
+      <div className="min-h-96 m-auto flex justify-center items-center">
+        <p>Sorry please check internet </p>
+      </div>
+    );
+  }
+  if (!data) return null;
+  return data?.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="grid lg:grid-flow-row lg:grid-cols-5 lg:row-span-3 lg:p-10 p-6 lg:gap-8 gap-5 m-auto items-center ">
       {data.map((data, index) => (
         <Link
-        to={'/bookdetails/'+ data.id}
+          to={"/bookdetails/" + data.id}
           key={index}
           className="flex flex-col border lg:w-64 w-full lg:gap-4 gap-2 m-auto shadow-xl border-gray-400  p-4 rounded-lg "
         >
           <img
             src={data.cover_image}
             alt="Book"
-            className="lg:w-56 w-full border border-red-500 rounded-lg lg:h-52 h-full object-cover transform transition-transform duration-300 hover:scale-105"
+            className="lg:w-56 w-full border border-red-500 rounded-lg h-52 object-cover transform transition-transform duration-300 hover:scale-105"
           />
           <div>
             <h2 className="text-lg text-black font-semibold capitalize">
               title :- {data.title}
             </h2>
             <div className="flex flex-row justify-between">
-            <p className="flex flex-row gap-2 items-center">
-                <span className="text-lg text-gray-900 font-semibold">₹{data.sale}</span>
+              <p className="flex flex-row gap-2 items-center">
+                <span className="text-lg text-gray-900 font-semibold">
+                  ₹{data.sale}
+                </span>
                 <span className="text-xs font-normal line-through">
                   ₹{data.price}
                 </span>
@@ -50,9 +59,10 @@ const Home = () => {
                   {data.discount} % OFF
                 </span>
               </p>
-              <span className="items-end"><FaCartPlus/></span>
-          </div>
-
+              <span className="items-end">
+                <FaCartPlus />
+              </span>
+            </div>
           </div>
         </Link>
       ))}
