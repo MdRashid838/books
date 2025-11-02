242c31949a875759f7e5c3c932cce3bce090f4a9
Anwarul07 <Officialmdanwarulhaque@gmail.com>
2025-11-02 16:26:36 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import { useState } from 'react';
-import Navbar from './components/pages/Navbar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-import Profile from './components/pages/Profile';
-import BookDetails from './components/pages/BookDetails';
-import Footer from './components/pages/Footer';
+import { useState } from "react";
+import Navbar from "./components/pages/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Profile from "./components/pages/Profile";
+import BookDetails from "./components/pages/BookDetails";
+import Footer from "./components/pages/Footer";
+import Logout from "./components/pages/Logout";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/profile' element={<Profile/>}/>
-      <Route path= '/bookdetails/:id' element={<BookDetails/>}/>
-    </Routes>
-    <Footer/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/bookdetails/:id" element={<BookDetails />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
